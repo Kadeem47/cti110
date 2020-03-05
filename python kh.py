373ab1d26289b0a9db727ac4d6a4d724c69355d5
Kadeem47 <hawkins.kadeem255@gmail.com>
2020-03-05 10:32:54 -0500

    Add files via upload
    First submission

---
+++ python kh.py
@@ -0,0 +1,25 @@
+##x = 4
+##y = 5
+##
+##if x>2 and x<5:
+##  print(x is > 2and less than 5")
+
+
+if x> 2 and x < 5:
+    print("x is > 2 and less than 5")
+
+
+
+#if 5 <x <2:
+
+
+value ='yes'
+
+while value.lower =='yes':
+    x = 4
+    y = 5
+
+    if x>2 and x<5:
+    print("x is > 2 and less)
+    value = input("Enter yes to run the program again:")
+    print("Program has ended")
